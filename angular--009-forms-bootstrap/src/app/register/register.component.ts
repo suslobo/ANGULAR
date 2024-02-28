ba885f7de52685bdb85de3550ea25a01eddb4481
suslobo <148755303+suslobo@users.noreply.github.com>
2024-02-28 16:42:11 +0100

    login registro

--- angular--009-forms-bootstrap/src/app/register/register.component.ts
+++ angular--009-forms-bootstrap/src/app/register/register.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Register } from '../interfaces/register.model';
 
 @Component({
@@ -13,32 +12,45 @@ import { Register } from '../interfaces/register.model';
 })
 export class RegisterComponent {
 
-  // 2. crear formulario
   registerForm = this.fb.group({
-    nickname: [''],
-    email: [''],
-    phone: [''],
-    password: ['']
-  });
+    nickname: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required, Validators.pattern('^[0-9]{9}$')]],
+    password: ['', [Validators.required]],
+    passwordConfirm: ['', [Validators.required]]
+  },
+  {validators: this.passwordConfirmValidator} // Validador personalizado para comprobar que las contraseñas son iguales
+  );
 
-  // 1. constructor
   constructor(private fb: FormBuilder,
     private httpClient: HttpClient) {}
 
-  // 3. metodo save
+    // Método personalizado para validar si la contraseña es igual a la confirmación de contraseña
+  passwordConfirmValidator(control: AbstractControl){
+    if(control.get('password')?.value === control.get('passwordConfirm')?.value){
+      return null; // si coinciden, son iguales y por tanto no hay error.
+    } else {
+      return {
+        'confirmError': true // si son distintas se devuelve error true
+      }
+    }
+  }
+
   save(){
+
     let register: Register = {
       nickname: this.registerForm.get('nickname')?.value ?? '',
       email: this.registerForm.get('email')?.value ?? '',
       phone: this.registerForm.get('phone')?.value ?? '',
       password: this.registerForm.get('password')?.value ?? '',
-    }
+    };
 
-  // 4. enviar POST para que llegue al backend
-  let url = 'http://localhost:3000/register';
-  this.httpClient.post<Register>(url, register).subscribe(res => {
-    console.log(res);
-  }); 
+    let url = 'http://localhost:3000/register';
+    this.httpClient.post<Register>(url, register)
+                    .subscribe(res => {
+                      console.log(res);
+                    });
+
+
   }
-
 }
